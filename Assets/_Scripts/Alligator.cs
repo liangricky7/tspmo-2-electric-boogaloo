3676321beab87f13b31b2f3f9dd1c82c31056da1
Ricky Liang <liangricky7@gmail.com>
2025-11-08 10:52:12 -0600

    Basic Alligator Minigame

--- Assets/_Scripts/Alligator.cs
+++ Assets/_Scripts/Alligator.cs
@@ -2,27 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crocodile : MonoBehaviour
+public class Alligator : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     bool isAttacking = false;
-    private int chanceToAttack = 3; // 1 in X chance each second
+    private int chanceToAttack = 40; // 1 in X chance each second
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);   
-        StartCoroutine(HitCroc());
+        spriteRenderer.color = new Color(1f, 1f, 1f, 0.2f);   
+        StartCoroutine(GatorCheck());
     }
 
-    IEnumerator CrocCheck()
+    IEnumerator GatorCheck()
     {
+        Debug.Log("enter check");
         while (!isAttacking)
         {
             yield return new WaitForSeconds(1f);
-            int chance = Random.Range(1, chanceToAttack); // independently defined for debugging purposes
+            int chance = Random.Range(0, chanceToAttack); // independently defined for debugging purposes
+            Debug.Log(chance);
             if (chance == 1)
             {
+                Debug.Log("enter attack");
                 isAttacking = true;
                 AttackBoat();
             }
@@ -31,14 +34,14 @@ public class Crocodile : MonoBehaviour
 
     void AttackBoat()
     {
-        Debug.Log("Croc Attacking!");
+        Debug.Log("Gator Attacking!");
         spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
-        StartCoroutine(HitCroc());
+        StartCoroutine(HitGator());
     }
 
-    IEnumerator HitCroc()
+    IEnumerator HitGator()
     {
-        int hitCount = Random.Range(10, 20);
+        int hitCount = Random.Range(10, 15);
 
         while (hitCount > 0)
         {
@@ -48,15 +51,18 @@ public class Crocodile : MonoBehaviour
                 spriteRenderer.color = new Color(1f, 0f, 0f, 1f);
                 yield return new WaitForSeconds(0.1f);
                 spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                Debug.Log("you diggin in me " + "; Hits remaining: " + hitCount);
             }
             yield return null;
         }
-        Retreat();
+        StartCoroutine(Retreat());
     }
     
-    void Retreat()
+    IEnumerator Retreat()
     {
         spriteRenderer.color = new Color(1f, 1f, 1f, 0.6f);
         isAttacking = false;
+        yield return new WaitForSeconds(2f); // grace period before next attack
+        StartCoroutine(GatorCheck());
     }
 }
