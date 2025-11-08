81f9038c0b8166d1597d239ab033a39f75e73244
Ricky Liang <liangricky7@gmail.com>
2025-11-08 13:10:09 -0600

    Placeholder ingredient sprites

--- Assets/LandIngredient.cs
+++ Assets/LandIngredient.cs
@@ -1,19 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LandIngredient : MonoBehaviour
 {
     string ingredientName = "Land Ingredient";
-    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    SpriteRenderer ingredientSpriteRenderer;
 
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
     public void SetIngredientInfo(string name, Sprite sprite)
     {
         ingredientName = name;
-        spriteRenderer.sprite = sprite;
+        ingredientSpriteRenderer.sprite = sprite;
     }
 }
--- Assets/_Scripts/LandSpawner.cs
+++ Assets/_Scripts/LandSpawner.cs
@@ -9,8 +9,11 @@ public class LandSpawner : MonoBehaviour
     private GameObject landIngredientPrefab;
 
     [SerializeField]
-    private Tuple<string, Sprite>[] landNameSprites;
+    private string [] landNames;
 
+    [SerializeField]
+    private Sprite[] landSprites;
+    
     private int chanceToSpawn = 4; // 1 in X chance each second
 
     void Start()
@@ -29,8 +32,8 @@ public class LandSpawner : MonoBehaviour
                 GameObject ingredient = Instantiate(landIngredientPrefab, new Vector3(transform.position.x, transform.position.y + UnityEngine.Random.Range(-3f, 3f), transform.position.z), Quaternion.identity);
                 
                 // assign random ingredient info
-                int randInd = UnityEngine.Random.Range(0, landNameSprites.Length);
-                ingredient.GetComponent<LandIngredient>().SetIngredientInfo(landNameSprites[randInd].Item1, landNameSprites[randInd].Item2);
+                int randInd = UnityEngine.Random.Range(0, landNames.Length);
+                ingredient.GetComponent<LandIngredient>().SetIngredientInfo(landNames[randInd], landSprites[randInd]);
                 
                 ingredient.GetComponent<Rigidbody2D>().velocity = new Vector2(-4f, 0f);
                 yield return new WaitForSeconds(2f); //grace period
