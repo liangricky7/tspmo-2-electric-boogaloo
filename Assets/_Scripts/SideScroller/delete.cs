d4ca0189e883dd0d3d6b92020725c9f3a7ef4831
JosiahMatriarch <corbinbrescher@gmail.com>
2025-11-10 11:59:14 -0600

    ricky can no longer be mad at me for my script naming

--- Assets/_Scripts/SideScroller/delete.cs
+++ Assets/_Scripts/SideScroller/delete.cs
@@ -12,7 +12,7 @@ public class Delete : MonoBehaviour
             scrollManager.panelList.Remove(parent);
             Destroy(parent);
             scrollManager.readyForeground = true;
-        } 
+        }
         else
         {
             scrollManager.panelList.Remove(parent);
